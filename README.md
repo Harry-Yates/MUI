535d02fb6f2ce7ed6326781ccbd92a3757254798
Harry-Yates <hyates1@gmail.com>
2023-05-03 15:45:17 +0200

    buttons done

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MUI Refresher 
 
 Timestamp: 10:00
+
+[Button Docs](https://mui.com/material-ui/react-button/)
+
+[Icons](https://mui.com/material-ui/material-icons/?query=football)
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
     <Button startIcon={<SportsSoccer />}  variant="text" size="small">Contained</Button>
     <Button startIcon={<Delete />} variant="contained" color="secondary" size="small">Contained</Button>
-    <Button startIcon={<Add />} variant="contained" color="success" size="small">Contained</Button>
+    <Button startIcon={<Add />} variant="contained" color="success" size="small">Add new post</Button>
     <Button startIcon={<Settings />}  variant="outlined" size="small" >Outlined</Button>
     </div>
   );
