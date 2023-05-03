cb7cf0af09bfe046e796e7eb104c27e9f986f682
Harry-Yates <hyates1@gmail.com>
2023-05-03 15:26:38 +0200

    docs: :zap: adding buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Button } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import SettingsIcon from '@mui/icons-material/Settings';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 
 function App() {
   return (
     <div className="App">
-    <Button variant="text">Text</Button>
-    <Button variant="contained">Contained</Button>
-    <Button variant="outlined">Outlined</Button>
+    <Button startIcon={<SportsSoccerIcon />}  variant="text" size="small">Contained</Button>
+    <Button startIcon={<DeleteIcon />} variant="contained" color="secondary" size="small">Contained</Button>
+    <Button startIcon={<SettingsIcon />}  variant="outlined" size="small" >Outlined</Button>
     </div>
   );
 }
