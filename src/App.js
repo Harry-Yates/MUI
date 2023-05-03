8da53d671932c32b3874762ee81781d90993a9fa
Harry-Yates <hyates1@gmail.com>
2023-05-03 15:39:46 +0200

    docs: :art: Worked more on buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Button } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import { Add, Settings, SportsSoccer, Delete } from "@mui/icons-material";
 
 function App() {
   return (
     <div className="App">
-    <Button startIcon={<SportsSoccerIcon />}  variant="text" size="small">Contained</Button>
-    <Button startIcon={<DeleteIcon />} variant="contained" color="secondary" size="small">Contained</Button>
-    <Button startIcon={<SettingsIcon />}  variant="outlined" size="small" >Outlined</Button>
+    <Button startIcon={<SportsSoccer />}  variant="text" size="small">Contained</Button>
+    <Button startIcon={<Delete />} variant="contained" color="secondary" size="small">Contained</Button>
+    <Button startIcon={<Add />} variant="contained" color="success" size="small">Contained</Button>
+    <Button startIcon={<Settings />}  variant="outlined" size="small" >Outlined</Button>
     </div>
   );
 }
